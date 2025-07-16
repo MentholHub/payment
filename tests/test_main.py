a8cfe24d1afda7aa7ac3d10137053cb3d7334fbe
witttccchhher <somokill650@gmail.com>
2025-07-16 22:03:31 +0500

    Fix CI error #3

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_ReadRoot():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"key": "value;"}
+    assert response.json() == {"key": "value"}
